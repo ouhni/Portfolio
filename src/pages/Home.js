b76afb88f28b30a0ac2c5af979fe0cad12aa8e2a
Yassine <youhni.123@gmail.com>
2024-05-21 19:57:39 -0400

    Finished styling Home Page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,32 @@
 import React from 'react';
-import { Container, Button } from 'react-bootstrap';
+import { Container, Row, Col, Button } from 'react-bootstrap';
+import profilePhoto from '../assets/images/Yassine_Profile_Photo.png';
+import '../styles/Home.css';
+
 const Home = () => {
   return (
-    <div className="bg-primary text-white text-center py-5">
+    <div className="home bg-primary text-white py-5">
       <Container>
-        <h1>Welcome to my Portfolio</h1>
-        <p>I am a passionate Frontend/Fullstack Engineer eager to contribute to impactful projects.</p>
-        <Button variant="primary" href="/resume">View Resume</Button>
+        <Row className="align-items-center">
+          <Col md={6} className="text-center mb-4 mb-md-0">
+            <img src={profilePhoto} alt="Portfolio_Photo" className="home-image" />
+          </Col>
+          <Col md={6} className="text-center text-md-left">
+          <h4>Hello, I'm</h4>
+          <h1>Yassine Ouhni</h1>
+          <h3>Full Stack Developer / Software Engineer</h3>
+          <h4>Based in Boston, Ma</h4>
+          <p>
+          I'm passionate about crafting dynamic web applications and seeking a developer or software engineer role to innovate and grow.
+          Skilled in HTML, CSS, JavaScript, Node.js, Express, React, SQL, Python, and Java, I thrive in collaborative environments and 
+          am committed to continuous learning. My aim is to contribute to impactful projects while advancing my career in tech.
+          </p>
+            <Button variant="light" href="/resume">View Resume</Button>
+          </Col>
+        </Row>
       </Container>
     </div>
   );
 }
+
 export default Home;
