a4e6643ef71aa10c28db36f8892730de3e0c875c
Yassine <youhni.123@gmail.com>
2024-05-28 21:03:34 -0400

    Add some styling & Download CV button

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Container, Row, Col, Button } from 'react-bootstrap';
 import profilePhoto from '../assets/images/Yassine_Profile_2_Photo.png'; // Adjust the path as needed
 import '../styles/About.css';
+import resume from '../assets/images/Yassine_Ouhni_Resume.pdf'; // Adjust the path as needed
 
 const About = () => {
   return (
@@ -15,20 +16,20 @@ const About = () => {
             <h1>About Me</h1>
 
             <p>
-            I'm Yassine, a recent Computer Science graduate deeply passionate about web development and software engineering. My journey into the world of technology began six years ago with an enriching experience at Year Up. There, as an IT technician, I immersed myself in hardware intricacies, laying the groundwork for my fascination with software development.
+              I'm Yassine, a recent Computer Science graduate deeply passionate about web development and software engineering. My journey into the world of technology began six years ago with an enriching experience at Year Up. There, as an IT technician, I immersed myself in hardware intricacies, laying the groundwork for my fascination with software development.
             </p>
             <p>
-            Transitioning to coding, I eagerly pursued a deeper understanding, which led me to Boston University. Through its comprehensive curriculum, I mastered programming languages and web development, fueling my passion and propelling me towards becoming a proficient software engineer.
+              Transitioning to coding, I eagerly pursued a deeper understanding, which led me to Boston University. Through its comprehensive curriculum, I mastered programming languages and web development, fueling my passion and propelling me towards becoming a proficient software engineer.
             </p>
             <p>
-            Now equipped with a solid foundation in both hardware and software, I'm excited to embark on the next chapter of my career. As a web developer or software engineer, I'm eager to leverage my diverse skill set and passion for technology to create innovative solutions that make a meaningful impact.
+              Now equipped with a solid foundation in both hardware and software, I'm excited to embark on the next chapter of my career. As a web developer or software engineer, I'm eager to leverage my diverse skill set and passion for technology to create innovative solutions that make a meaningful impact.
             </p>
             <p>
-            Beyond my academic and professional pursuits, I enjoy soccer, experimenting with cuisines in the kitchen, and leisurely walks under the sun.  
+              Beyond my academic and professional pursuits, I enjoy soccer, experimenting with cuisines in the kitchen, and leisurely walks under the sun.
             </p>
 
             <div className="button-group mt-4">
-              <Button variant="primary" href="/path/to/cv" className="mr-2">Download CV</Button>
+              <a href={resume} download="Yassine_Ouhni_Resume.pdf" className="btn btn-primary mr-2">Download CV</a>
               <Button variant="secondary" href="/projects" className="mr-2">My Projects</Button>
               <Button variant="success" href="/contact">Contact Me</Button>
             </div>
